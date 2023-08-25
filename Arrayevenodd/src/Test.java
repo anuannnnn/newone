f0eddb9f8891b41054f21506b533d1939d28098f
hhhkk <ancyrajan7382@gmail.com>
2023-08-25 23:58:36 +0530

    ccccc

---
+++ Arrayevenodd/src/Test.java
@@ -0,0 +1,36 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a[]={2,3,6,1,7,9};
+	
+		
+			for(int j=0;j<a.length;j++)
+			{
+				if(a[j]%2==0)
+				{
+					System.out.println(a[j]);
+					
+				}
+				
+				
+			}
+			System.out.println("odd numbers");
+			for(int j=0;j<a.length;j++)
+			{
+				if(a[j]%2!=0)
+				{
+					System.out.println(a[j]);
+					
+				}
+				
+				
+			}
+		}
+		
+	}
+
+
